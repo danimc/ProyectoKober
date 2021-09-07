e7261d7ac8b45998f88c990282bfccdd4a6681f2
OAG HQ <daniel_k310a@hotmail.com>
2021-09-07 15:50:02 -0500

    payouts a meses

--- TMEPortal/TMEPortal/Controllers/PayoutsController.cs
+++ TMEPortal/TMEPortal/Controllers/PayoutsController.cs
@@ -1,15 +1,32 @@
 ﻿using Newtonsoft.Json;
 using Stripe;
-using Stripe.Checkout;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
+//using Stripe.Checkout;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Stripe.Checkout;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TMEPortal.Controllers
 {
+    public class CollectDetailsRequest
+    {
+        [JsonProperty("payment_method_id")]
+        public string PaymentMethodId { get; set; }
+    }
+
+    public class ConfirmPaymentRequest
+    {
+        [JsonProperty("payment_intent_id")]
+        public string PaymentIntentId { get; set; }
+
+        [JsonProperty("selected_plan")]
+        public PaymentIntentPaymentMethodOptionsCardInstallmentsPlan SelectedPlan { get; set; }
+    }
+
+    [System.Web.Http.Route("/")]
     public class PayoutsController : Controller
     {
 
@@ -19,7 +36,7 @@ namespace TMEPortal.Controllers
                 "sk_test_51JPs4jHcWweoTFXWqVGuSxA8W8OAzS6c9N2IF6YDwiqy4lSGq1yjg5ayU0sBt3kR5Jo4NZ2EjxCe5pI8AX2lJT2v00khXvkPkq";
         }
 
-        [HttpPost()]
+        [System.Web.Http.HttpPost()]
         public ActionResult CreateCheckoutSession()
         {
             var options = new SessionCreateOptions
@@ -64,8 +81,8 @@ namespace TMEPortal.Controllers
             return View();
         }
 
-        [HttpGet]
-        public void exito ()
+        [System.Web.Http.HttpGet]
+        public void Exito()
         {
             Response.ContentType = "application/json";
             StripeConfiguration.ApiKey = "sk_test_51JPs4jHcWweoTFXWqVGuSxA8W8OAzS6c9N2IF6YDwiqy4lSGq1yjg5ayU0sBt3kR5Jo4NZ2EjxCe5pI8AX2lJT2v00khXvkPkq";
@@ -84,9 +101,95 @@ namespace TMEPortal.Controllers
             Response.Write(json);
 
 
-           // return View("Index");
+            // return View("Index");
+        }
+
+
+        //[System.Web.Http.Route("/collect_details")]
+       // [System.Web.Http.HttpPost]
+
+        public async Task<ActionResult> Datos([FromBody] CollectDetailsRequest request)
+        {
+            var service = new PaymentIntentService();
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = 30000,
+                Currency = "mxn",
+                PaymentMethodTypes = new List<string> { "card" },
+                PaymentMethod = request.PaymentMethodId,
+                PaymentMethodOptions = new PaymentIntentPaymentMethodOptionsOptions
+                {
+                    Card = new PaymentIntentPaymentMethodOptionsCardOptions
+                    {
+                        Installments = new PaymentIntentPaymentMethodOptionsCardInstallmentsOptions
+                        {
+                            Enabled = true,
+                        },
+                    },
+                },
+            };
+
+            try
+            {
+                var intent = await service.CreateAsync(options);
+
+                return Json(new
+                {
+                    intent_id = intent.Id,
+                    available_plans = intent.PaymentMethodOptions.Card.Installments.AvailablePlans,
+                });
+            }
+            catch (StripeException e)
+            {
+                // "e" contains a message explaining why the request failed
+                
+                int estatus =  (int)e.HttpStatusCode;
+                return Json(new {error = estatus, data = e.StripeError.Message });
+            }
+        }
+
+        public async Task<ActionResult> Confirma([FromBody] ConfirmPaymentRequest request)
+        {
+            var service = new PaymentIntentService();
+
+            try
+            {
+                var options = new PaymentIntentConfirmOptions();
+
+                if (request.SelectedPlan != null)
+                {
+                    options.PaymentMethodOptions = new PaymentIntentPaymentMethodOptionsOptions
+                    {
+                        Card = new PaymentIntentPaymentMethodOptionsCardOptions
+                        {
+                            Installments = new PaymentIntentPaymentMethodOptionsCardInstallmentsOptions
+                            {
+                                Enabled = true,
+                                Plan = new PaymentIntentPaymentMethodOptionsCardInstallmentsPlanOptions
+                                {
+                                    Count = request.SelectedPlan.Count,
+                                    Type = request.SelectedPlan.Type,
+                                    Interval = request.SelectedPlan.Interval,
+                                },
+                            },
+                        },
+                    };
+                }
+
+                var intent = await service.ConfirmAsync(request.PaymentIntentId, options);
+
+                return Json(new
+                {
+                    status = intent.Status,
+                });
+            }
+            catch (StripeException e)
+            {
+                // "e" contains a message explaining why the request failed
+                int estatus = (int)e.HttpStatusCode;
+                return Json(new { error = estatus, data = e.StripeError.Message });
+            }
         }
     }
-
-
+    
 }
